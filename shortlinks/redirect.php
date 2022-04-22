b06297bb596a9cc93cf42c682746f2b4c53124e7
Simeon Platonov <simeon.platonov@gmail.com>
2022-04-22 15:36:10 +0300

    Enabled redirecting

---
+++ shortlinks/redirect.php
@@ -0,0 +1,27 @@
+<?php
+  namespace Shortlinks;
+  
+  if (!defined('ABSPATH')) { 
+    exit;
+  }
+  
+  add_action ('init', function () {
+    $path = $_SERVER['REQUEST_URI'];
+    
+    $args = [
+      'fields'=> 'ids',
+      'post_type'=> 'shortlink',
+      'meta_key'=> 'path',
+      'meta_value'=> $path
+    ];
+    
+    $id = get_posts($args)[0];
+    if ($id) {
+      $url = get_post_meta($id, "url", true);
+      if ($url) {
+        wp_redirect($url);
+         exit;
+      }
+    } 
+  });  
+?>
