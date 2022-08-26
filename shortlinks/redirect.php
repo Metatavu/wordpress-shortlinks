6f43e1312729c376a413bec6ebc8464778c288b7
Antti Leppä <antti.leppa@metatavu.fi>
2022-08-26 12:01:34 +0300

    Fixed redirect issue

--- shortlinks/redirect.php
+++ shortlinks/redirect.php
@@ -7,21 +7,22 @@
   
   add_action ('init', function () {
     $path = $_SERVER['REQUEST_URI'];
-    
-    $args = [
-      'fields'=> 'ids',
-      'post_type'=> 'shortlink',
-      'meta_key'=> 'path',
-      'meta_value'=> $path
-    ];
-    
-    $id = get_posts($args)[0];
-    if ($id) {
-      $url = get_post_meta($id, "url", true);
-      if ($url) {
-        wp_redirect($url);
-        exit;
-      }
-    } 
-  });  
+    if ($path) {
+      $args = [
+        'fields'=> 'ids',
+        'post_type'=> 'shortlink',
+        'meta_key'=> 'path',
+        'meta_value'=> $path
+      ];
+      
+      $id = get_posts($args)[0];
+      if ($id) {
+        $url = get_post_meta($id, "url", true);
+        if ($url) {
+          wp_redirect($url);
+          exit;
+        }
+      } 
+    });  
+  }
 ?>
